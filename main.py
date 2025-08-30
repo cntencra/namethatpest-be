36e468730cbaac9fabdc865e12a476cb2ad548df
cntencra <cn10cra@gmail.com>
2025-08-30 15:25:05 +0100

    fix: update cors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from fastapi_backend.db.db_manager import db_manager
 from fastapi_backend.app.routes import api
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,5 +19,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",
+    "http://192.168.0.14:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.router, prefix="/api")
